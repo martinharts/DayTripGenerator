f8388def700ea99fc4498b81774eab8f1a3ecc24
martinharts <martin_harts@yahoo.com>
2021-08-19 18:53:58 -0400

    created functions for random selections of arrays. created function to let user re-select choices

--- index.js
+++ index.js
@@ -5,7 +5,66 @@ let rest = ["Chinese", "American", "Italian", "Japanese"];
 let transpo = ["Car", "Train", "Plane", "Boat"]
 let enter = ["Play", "Movie", "Sport Event", "Bar"]
 
-let value = math.floor(math.random() * dest.length);
-let rRest = math.floor(math.random() * rest.length);
-let rTrans = math.floor(math.random() * transpo.length);
-let rEnter = math.floor(math.random() * enter.length);
+function destination(){
+    let randomDest = Math.floor(Math.random()*dest.length);
+    return dest[randomDest]
+}
+function restuarant(){
+    let randomRest = Math.floor(Math.random()*rest.length)
+    return rest[randomRest]
+}
+function transportation(){
+    let randomTrans = Math.floor(Math.random()*transpo.length)
+    return transpo[randomTrans]
+}
+function entertainment(){
+    let randomEnter = Math.floor(Math.random()*enter.length)
+    return enter[randomEnter]
+}
+
+let rDest = destination();
+let rRest = restuarant();
+let rTrans = transportation();
+let rEnter = entertainment();
+
+
+
+function randomTrip(str){
+    if(str === "1"){
+        let newDest = rDest;
+        while(curDest == newDest){
+            newDest = rDest;
+        }
+    }
+    if(curDest != newDest){
+        curDest = newDest;
+    }
+    else if(str === "2"){
+        let newRes = rRest;
+        while(curRest == newRes){
+            newRes = rRest;
+        }
+        if(curRest != newRes){
+            curRest = newRes;
+        }
+    }
+    else if(str === "3"){
+        let newTrans = rTrans;
+        while(curTrans == newTrans){
+            newTrans = rTrans;
+        }
+        if(curTrans != newTrans){
+            curTrans = newTrans;
+        }
+    }
+    else if(str === "4"){
+        let newEnter = rEnter;
+        while(curEnter == newEnter){
+            newEnter = rEnter;
+        }
+        if(curEnter != newEnter){
+            curEnter = newEnter
+        }
+    }
+}
+
