a9ff72657040831ec3493be2775310d3e1fba503
martinharts <martin_harts@yahoo.com>
2021-08-18 20:36:28 -0400

    added values for random numbers generated from arrays

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ let dest = ["Hawaii", "California", "New York", "Chicago"];
 let rest = ["Chinese", "American", "Italian", "Japanese"];
 let transpo = ["Car", "Train", "Plane", "Boat"]
 let enter = ["Play", "Movie", "Sport Event", "Bar"]
+
+let value = math.floor(math.random() * dest.length);
+let rRest = math.floor(math.random() * rest.length);
+let rTrans = math.floor(math.random() * transpo.length);
+let rEnter = math.floor(math.random() * enter.length);
