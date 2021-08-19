29ab5d4b563f272414d5af5ebf1eee921a34cc52
martinharts <martin_harts@yahoo.com>
2021-08-18 20:28:32 -0400

    created arrays for destination, restuarant, transportation, and entertainment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 "use strict"
+
+let dest = ["Hawaii", "California", "New York", "Chicago"];
+let rest = ["Chinese", "American", "Italian", "Japanese"];
+let transpo = ["Car", "Train", "Plane", "Boat"]
+let enter = ["Play", "Movie", "Sport Event", "Bar"]
