fe06a1d586e3c5eec477a0fbac6b981f7055f350
martinharts <martin_harts@yahoo.com>
2021-08-19 22:18:34 -0400

    refactored array functions into one array function and added more notes

--- index.js
+++ index.js
@@ -5,30 +5,36 @@ let rest = ["Chinese", "American", "Italian", "Japanese"];
 let transpo = ["Car", "Train", "Plane", "Boat"]
 let enter = ["Play", "Movie", "Sport Event", "Bar"]
 // functions to generate random index number for arrays
-function destination(){
-    let randomDest = Math.floor(Math.random()*dest.length);
-    return dest[randomDest]
-}
-function restuarant(){
-    let randomRest = Math.floor(Math.random()*rest.length)
-    return rest[randomRest]
-}
-function transportation(){
-    let randomTrans = Math.floor(Math.random()*transpo.length)
-    return transpo[randomTrans]
-}
-function entertainment(){
-    let randomEnter = Math.floor(Math.random()*enter.length)
-    return enter[randomEnter]
+// initial functions for random array 
+// function destination(){
+//     let randomDest = Math.floor(Math.random()*dest.length);
+//     return dest[randomDest]
+// }
+// function restuarant(){
+//     let randomRest = Math.floor(Math.random()*rest.length)
+//     return rest[randomRest]
+// }
+// function transportation(){
+//     let randomTrans = Math.floor(Math.random()*transpo.length)
+//     return transpo[randomTrans]
+// }
+// function entertainment(){
+//     let randomEnter = Math.floor(Math.random()*enter.length)
+//     return enter[randomEnter]
+// }
+// refactored into one function for multiple arrays
+function randomArrayItem(array){
+    let randomItem = array[Math.floor(Math.random() * array.length)];
+    return randomItem
 }
 // call for functions 
-let rDest = destination();
-let rRest = restuarant();
-let rTrans = transportation();
-let rEnter = entertainment();
+let rDest = randomArrayItem(dest)
+let rRest = randomArrayItem(rest)
+let rTrans =randomArrayItem(transpo)
+let rEnter = randomArrayItem(enter)
 //function to prompt user asking to confirm or deny generated trip
 function reDo(){
-    let again = prompt(" Is your trip okay? Enter Y or N")
+    let again = prompt(" Is your trip okay? Enter Y or N").toLowerCase()
     return again;
 }
 //alert for initial generated trip 
@@ -39,18 +45,17 @@ alert(info)
 
 let choice = "n"
 // while loop with conditions for user input of Y or N to generate new trip and console.log acceptance
-while(choice != "Y" || choice != "y"){
+while(choice != "y"){
     choice = reDo();
-    if(choice === "Y" || choice === "y"){
+    if(choice === "y"){
         alert("Enjoy your trip!");
         break;
     }
-    else if (choice === "N" || choice === "n"){
-        let rDest = destination();
-        let rRest = restuarant();
-        let rTrans = transportation();
-        let rEnter = entertainment();
-
+    else if (choice === "n"){
+        let rDest = randomArrayItem(dest)
+        let rRest = randomArrayItem(rest)
+        let rTrans =randomArrayItem(transpo)
+        let rEnter = randomArrayItem(enter)
         let newInfo = "Your new trip is in " + rDest + ". Eating " + rRest + ". Riding in a " + rTrans + ", and going to a " + rEnter + "."
         alert(newInfo);
         console.log(newInfo);
